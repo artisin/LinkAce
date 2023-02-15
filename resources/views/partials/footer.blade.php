63386d75b75784ed47d411374308b4c92ab8760f
te <github@teschultz.com>
2023-02-15 17:53:17 -0600

    dev build foot

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,11 +1,5 @@
 <aside class="footer container text-center small pt-3 pb-5">
     <div>
-        @lang('linkace.project_of') <a href="https://kovah.de/?utm_source=linkace" rel="noopener" target="_blank">Kovah.de</a>
+        DEV_BUILD | @lang('linkace.version', ['version' => \App\Helper\UpdateHelper::currentVersion()]) | <a href="https://kovah.de/?utm_source=linkace" rel="noopener" target="_blank">Kovah.de</a>
     </div>
-    @auth
-        <div class="mt-1">
-            @lang('linkace.version', ['version' => \App\Helper\UpdateHelper::currentVersion()]) -
-            <x-update-check class="d-inline-block"/>
-        </div>
-    @endauth
 </aside>
